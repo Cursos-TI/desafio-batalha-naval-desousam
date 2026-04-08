1569d0a0a95511568509d26ab98872d0b68f07db
de Sousa <mariliacs0239@gmail.com>
2026-04-08 12:19:34 +0000

    corrigindo erro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,7 +7,7 @@ int main() {
     int tabuleiro [10][10];
 
     for (int i = 0; i < 10; i++){
-        for (j = 0; j < 10; j++){
+        for (int j = 0; j < 10; j++){
             tabuleiro[i][j] = 0;
         }
     }
