128ca57a476d89271aacfa6648fc1e9dc56803cc
de Sousa <mariliacs0239@gmail.com>
2026-04-08 12:18:33 +0000

    exibicao do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -40,5 +40,15 @@ int main() {
         }
     }
 
+    // ========== EXIBIÇÃO DO TABULEIRO ==========
+    printf("=== TABULEIRO ===\n\n");
+
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
